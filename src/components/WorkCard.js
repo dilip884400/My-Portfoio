cf9c97a707f6f6132da516a27ffe1c166af96749
Dilip Lovevanshi <113449567+dilip884400@users.noreply.github.com>
2023-07-03 03:55:37 +0530

    Update WorkCard.js

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -4,22 +4,40 @@ import { NavLink } from 'react-router-dom'
 
 function WorkCard(props) {
   return (
-    <div className='project-card'>
-    <img src={props.imgsrc} alt="img"/>
-    <h2 className='project-title'>{props.title}</h2>
-    <div className='pro-details'>
-    <ul className='pro-text12'>
-    <li>{props.text1}</li>
-      <li>{props.text2}</li>
-    </ul>
-      
-      <div className='pro-btns'>
-        <NavLink to={props.view} className="btn">View</NavLink>
-        <NavLink to={props.source} className="btn">Source</NavLink>
+    <div className="card-container">
+      <div className="project-card">
+        <img src={props.imgsrc} alt="img" />
+        <h2 className="project-title">{props.title}</h2>
+        <div className="pro-details">
+          <ul className="pro-text12">
+            <li>{props.text1}</li>
+            <li>{props.text2}</li>
+          </ul>
+
+          <div className="pro-btns">
+            <NavLink to={props.view} className="btn">
+              View
+            </NavLink>
+            <NavLink to={props.source} className="btn">
+              Source
+            </NavLink>
+          </div>
+        </div>
+        <div class="layers">
+          <div class="layer"></div>
+          <div class="layer"></div>
+          <div class="layer"></div>
+          <div class="layer"></div>
+          <div class="layer"></div>
+          <div class="layer"></div>
+          <div class="layer"></div>
+          <div class="layer"></div>
+          <div class="layer"></div>
+          <div class="layer"></div>
+        </div>
       </div>
     </div>
-</div>
-  )
+  );
 }
 
 export default WorkCard
