ee16c79662ebe905e91f82e4d541d66c4724ca8c
Dilip Lovevanshi <113449567+dilip884400@users.noreply.github.com>
2023-07-03 03:54:32 +0530

    Update WorkCardData.js

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -7,7 +7,7 @@ const ProjectCardData = [
     {
         imgsrc:pro1,
         title:"Google-Doc Clone",
-        text1:"This projects aim is to develop a Google-Docs-Clone app for users to create document.",
+        text1:"This is a Web-App created to provide users a Text area for writing their notes, thought It has all the features of formatting, customizing, and adding images according to our need. We can print the sheet with a click We can also convert the sheet into a compressed PDF for easy sharing.",
         text2:"Tech stack used : HTML, CSS, React.js, JavaScript, React-router-dom",
         view:"https://google-doc-3sir.onrender.com/",
         source:"https://github.com/the-chand-babu/GoogleDoc.git"
@@ -15,7 +15,7 @@ const ProjectCardData = [
     {
         imgsrc:pro2,
         title:"Gym Website",
-        text1:"This project aims to design and develop a modern and user-friendly website for a gym.",
+        text1:"This is a promotional static website for representing our gym across the people. User can go through the website to know about our faculties, membership & many more user can experience our website seamlessly from any device.",
         text2:"Tech stack used : HTML, CSS, React.js, JavaScript, React-router-dom",
         view:"https://gym-website-13y6.onrender.com/",
         source:"https://github.com/the-chand-babu/Project1_Gym.git"
@@ -23,7 +23,7 @@ const ProjectCardData = [
     {
         imgsrc:pro3,
         title:"KanBan Board",
-        text1:"This project aims to design and develop task management website.",
+        text1:"This project is a Web-App that provides a visual representation of tasks and workflow By using cards and columns to represent tasks and their progress We can add descriptions on any particular task according to our requirement. We can customize our board with any image across the internet.",
         text2:"Tech stack used : HTML, CSS, React, JavaScript, React-router-dom, Redux, Material-UI, React Beautiful DND.",
         view:"https://kanban-ikee.onrender.com/",
         source:"https://github.com/Ankush123rai/React-Kanban_Board.git"
@@ -31,7 +31,7 @@ const ProjectCardData = [
     {
         imgsrc:pro4,
         title:"Twitter Clone",
-        text1:"This project purpose is to design the Front-end of a Twitter website. ",
+        text1:"This project purpose is to design the Front-end of a Twitter website. This project is a clone of twitter where user can register and login Also we can able to add tweet. User can access the website form any device Smartphone, Tablets, PC.",
         text2:"Tech stack used : HTML, CSS, React.js, JavaScript, React-router-dom, Redux, Material-UI.",
         view:"https://twitter-clone-777.onrender.com/",
         source:"https://github.com/dilip884400/Twitter_clone.git"
